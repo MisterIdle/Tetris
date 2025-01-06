8dfb750590cf21023a569dba00ee84c469ee7153
MisterIdle <alexyhoublo@gmail.com>
2025-01-06 03:38:27 +0100

    Collision, movement, rotation, fall added

--- logic/GameLoop.cs
+++ logic/GameLoop.cs
@@ -9,18 +9,19 @@ namespace Tetris
 {
     class GameLoop
     {
-        public const int screenWidth = 800;
-        public const int screenHeight = 600;
-        public const int gridWidth = 10;
-        public const int gridHeight = 20;
-        public const int cellSize = 30;
-        public const int marginX = 50;
-        public const int marginY = 0;
+        public const int SCREEN_WIDTH = 800;
+        public const int SCREEN_HEIGHT = 600;
+        public const int GRID_WIDTH = 10;
+        public const int GRID_HEIGHT = 20;
+        public const int CELL_SIZE = 30;
+        public const int MARGIN_X = 50;
+        public const int MARGIN_Y = 0;
+
+        public static float deltaTime = 0.0f;
 
         public List<Block> blocks;
         public Block currentBlock;
         public Block nextBlock;
-
 
         public GameLoop()
         {
@@ -31,13 +32,17 @@ namespace Tetris
 
         public void Run()
         {
-            Raylib.InitWindow(screenWidth, screenHeight, "Tetris Game");
+            Raylib.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Tetris Game");
 
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Raylib.RAYWHITE);
 
+                deltaTime = Raylib.GetFrameTime();
+
+                Input();
+                Update();
                 Draw();
 
                 Raylib.EndDrawing();
@@ -45,38 +50,59 @@ namespace Tetris
             Raylib.CloseWindow();
         }
 
-        public void Draw() {
+        // INPUT AND DRAWING //
+        public void Input()
+        {
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+                currentBlock.RotateTetromino();
+
+
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))
+                currentBlock.MoveTetromino(-1, 0);
+
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT))
+                currentBlock.MoveTetromino(1, 0);
+
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                currentBlock.SetFallSpeed(0.07f);
+            else
+                currentBlock.SetFallSpeed(1.0f);
+        }
+
+        public void Update()
+        {
+            currentBlock.FallTetromino();
+        
+        }
+
+        public void Draw()
+        {
             DrawGrid();
             currentBlock.DrawTetromino();
         }
 
-        //////////
-        // Grid //
-        //////////
         public void DrawGrid()
         {
-            for (int i = 0; i < gridWidth; i++)
+            for (int i = 0; i < GRID_WIDTH; i++)
             {
-                for (int j = 0; j < gridHeight; j++)
+                for (int j = 0; j < GRID_HEIGHT; j++)
                 {
-                    Raylib.DrawRectangle(marginX + i * cellSize, marginY + j * cellSize, cellSize, cellSize, Raylib.LIGHTGRAY);
-                    Raylib.DrawRectangleLines(marginX + i * cellSize, marginY + j * cellSize, cellSize, cellSize, Raylib.DARKGRAY);
+                    Raylib.DrawRectangle(MARGIN_X + i * CELL_SIZE, MARGIN_Y + j * CELL_SIZE, CELL_SIZE, CELL_SIZE, Raylib.LIGHTGRAY);
+                    Raylib.DrawRectangleLines(MARGIN_X + i * CELL_SIZE, MARGIN_Y + j * CELL_SIZE, CELL_SIZE, CELL_SIZE, Raylib.DARKGRAY);
                 }
             }
         }
 
-        ///////////////
-        // Tetromino //
-        ///////////////
+        // BLOCK GENERATION //
         public Block GenerateRandomBlock()
         {
             Random random = new Random();
             int index = random.Next(blocks.Count);
             Block randomBlock = blocks[index];
 
-            return new Block(gridWidth / 2 - randomBlock.shape.GetLength(1) / 2, 0, randomBlock.shape, randomBlock.color);
+            return new Block(GRID_WIDTH / 2 - randomBlock.shape.GetLength(1) / 2, 0, randomBlock.shape, randomBlock.color);
         }
-
     }
 
     class Block
@@ -86,6 +112,9 @@ namespace Tetris
         public int[,] shape;
         public Color color;
 
+        public float fallTimer = 0.0f;
+        public float fallInterval = 1.0f;
+
         public Block(int x, int y, int[,] shape, Color color)
         {
             this.x = x;
@@ -94,6 +123,7 @@ namespace Tetris
             this.color = color;
         }
 
+        // JSON LOADING //
         public static List<Block> LoadFromJson(string filePath)
         {
             string jsonContent = File.ReadAllText(filePath);
@@ -112,6 +142,7 @@ namespace Tetris
         }
 
         // .NET 5.0 requires a conversion because [,] is not supported. (Developer note 😉)
+        // .NET 5.0 < 8.0
         private static int[,] ConvertTo2DArray(int[][] jaggedArray)
         {
             int rows = jaggedArray.Length;
@@ -129,7 +160,7 @@ namespace Tetris
             return array;
         }
 
-
+        // DRAWING AND MOVEMENT //
         public void DrawTetromino()
         {
             for (int i = 0; i < shape.GetLength(0); i++)
@@ -138,14 +169,104 @@ namespace Tetris
                 {
                     if (shape[i, j] == 1)
                     {
-                        Raylib.DrawRectangle(GameLoop.marginX + (x + j) * GameLoop.cellSize, GameLoop.marginY + (y + i) * GameLoop.cellSize, GameLoop.cellSize, GameLoop.cellSize, color);
-                        Raylib.DrawRectangleLines(GameLoop.marginX + (x + j) * GameLoop.cellSize, GameLoop.marginY + (y + i) * GameLoop.cellSize, GameLoop.cellSize, GameLoop.cellSize, Raylib.DARKGRAY);
+                        Raylib.DrawRectangle(GameLoop.MARGIN_X + (x + j) * GameLoop.CELL_SIZE, GameLoop.MARGIN_Y + (y + i) * GameLoop.CELL_SIZE, GameLoop.CELL_SIZE, GameLoop.CELL_SIZE, color);
+                        Raylib.DrawRectangleLines(GameLoop.MARGIN_X + (x + j) * GameLoop.CELL_SIZE, GameLoop.MARGIN_Y + (y + i) * GameLoop.CELL_SIZE, GameLoop.CELL_SIZE, GameLoop.CELL_SIZE, Raylib.DARKGRAY);
                     }
                 }
             }
         }
-    }
 
+        public void MoveTetromino(int dx, int dy)
+        {
+            int prevX = x;
+            int prevY = y;
+
+            x += dx;
+            y += dy;
+
+            if (CheckCollision())
+            {
+                x = prevX;
+                y = prevY;
+            }
+        }
+
+        public void FallTetromino()
+        {
+            fallTimer += GameLoop.deltaTime;
+
+            if (fallTimer >= fallInterval)
+            {
+                y++;
+                fallTimer = 0.0f;
+
+                if (CheckCollision())
+                {
+                    y--;
+                }
+            }
+        }
+
+        public void SetFallSpeed(float speed)
+        {
+            fallInterval = speed;
+        }
+
+        public void RotateTetromino()
+        {
+            int[,] rotatedShape = new int[shape.GetLength(1), shape.GetLength(0)];
+
+            for (int i = 0; i < shape.GetLength(0); i++)
+            {
+                for (int j = 0; j < shape.GetLength(1); j++)
+                {
+                    rotatedShape[j, shape.GetLength(0) - 1 - i] = shape[i, j];
+                }
+            }
+
+            int[,] prevShape = shape;
+            shape = rotatedShape;
+
+            if (CheckCollision())
+            {
+                shape = prevShape;
+            }
+        }
+        
+
+        // COLLISION DETECTION //
+        public bool CheckCollision()
+        {
+            for (int i = 0; i < shape.GetLength(0); i++)
+            {
+                for (int j = 0; j < shape.GetLength(1); j++)
+                {
+                    if (shape[i, j] == 1)
+                    {
+                        int newX = x + j;
+                        int newY = y + i;
+
+                        if (newX < 0 || newX >= GameLoop.GRID_WIDTH || newY >= GameLoop.GRID_HEIGHT)
+                        {
+                            return true;
+                        }
+
+                        if (IsCellOccupied(newX, newY))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsCellOccupied(int x, int y)
+        {
+            return false;
+        }
+    }
 
     class TetrominoData
     {
