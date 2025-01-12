42492f92398e39f6a71e2c65f1092b343e73acb2
MisterIdle <alexyhoublo@gmail.com>
2025-01-12 23:16:23 +0100

    Technical Doc added

--- obj/Debug/net5.0/Tetris.AssemblyInfo.cs
+++ obj/Debug/net5.0/Tetris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee2ef90910ecb6056960320dbe250fcc75fc085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24108cb1fcd2826808971166cb97009c50f761cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
